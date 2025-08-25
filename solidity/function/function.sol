72d05e92e64029a6d3bf1262cb502a13d313a1ff
Maiquel Paiva <untwlearn@gmail.com>
2025-08-25 07:40:50 -0300

    Update function.sol

--- solidity/function/function.sol
+++ solidity/function/function.sol
@@ -1,12 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.30;
+pragma solidity ^0.8.30;
 
 contract FunctionIntro {
-    function add(uint256 x, uint256 y) external pure returns (uint256) {
-        return x + y;
+    function add(uint256 x, uint256 y) external pure returns (uint256 result) {
+        unchecked {
+            result = x + y;
+        }
     }
 
-    function sub(uint256 x, uint256 y) external pure returns (uint256) {
-        return x - y;
+    error UnderflowError();
+
+    function sub(uint256 x, uint256 y) external pure returns (uint256 result) {
+        if (y > x) revert UnderflowError();
+        unchecked {
+            result = x - y;
+        }
     }
 }
